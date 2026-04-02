00f7a52d12033e3ecec7ef9fcd308508cc49f863
Veronika Gachkova <113029091+zamarunchik@users.noreply.github.com>
2026-04-02 16:19:45 +0300

    fix: remove hardcoded JWT token

--- frontend/src/app/app.guard.spec.ts
+++ frontend/src/app/app.guard.spec.ts
@@ -35,7 +35,7 @@ describe('LoginGuard', () => {
   }))
 
   it('returns payload from decoding a valid JWT', inject([LoginGuard], (guard: LoginGuard) => {
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c')
+    localStorage.setItem('token', 'MOCK_TOKEN_FOR_TESTING')
     expect(guard.tokenDecode()).toEqual({
       sub: '1234567890',
       name: 'John Doe',
